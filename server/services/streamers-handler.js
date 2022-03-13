40a77247eaa15bc079619ddc631ef56fcd9fdfdb
PAPAZIAN Maxime <maxime.papazian@etu.univ-amu.fr>
2022-03-13 17:12:59 +0100

    Correction des erreurs d'accès à la bdd

--- server/services/streamers-handler.js
+++ server/services/streamers-handler.js
@@ -1,70 +1,62 @@
 import streamersRepository from './streamers-repository';
 
 async function getStreamers(req, res) {
-    try {
-        const result = await streamersRepository.getAll();
-        const finalArray = [];
-        for (let obj of result.hits.hits) {
-            finalArray.push(obj._source);
-        }
-        res.send(finalArray);
-    } catch (e) {
-        res.status(400).end();
+  try {
+    const result = await streamersRepository.getAll();
+    const finalArray = [];
+    for (let obj of result.body.hits.hits) {
+      finalArray.push(obj.source);
     }
+    res.send(finalArray);
+  } catch (e) {
+    res.status(400).end();
+  }
 }
-
 
 async function create(req, res) {
-    res.set('Content-Type', 'application/json');
-    try {
-        //le boolean a l'air de marcher
-        const streamerBool = await streamerExist(req.body.streamerName);
-
-
-        if (streamerBool) {
-            res.send({});
-        } else {
-            //req.body marche
-            await streamersRepository.store(req.body);
-
-            res.send(
-                streamerName = 'ok'
-            );
-        }
-    } catch (e) {
-        res.status(400).end();
+  res.set('Content-Type', 'application/json');
+  try {
+    const streamerBool = await streamerExist(req.body.streamerName);
+    if (streamerBool) {
+      res.send({});
+    } else {
+      await streamersRepository.store(req.body);
+      res.send((streamerName = 'ok'));
     }
+  } catch (e) {
+    res.status(400).end();
+  }
 }
-//ne marche pas
+
 async function streamerExist(streamerName) {
-    try {
-        console.log("nom du streamer donné" + streamerName);
-        const result = await streamersRepository.getStreamer(streamerName);
-        return result ? true : false;
-    } catch (e) {
-        console.log('error getting streamer', e);
-        return false;
-    }
+  try {
+    const result = await streamersRepository.getStreamer(streamerName);
+    console.log(result);
+    return result.hits.total.value > 0 ? true : false;
+  } catch (e) {
+    console.log('error getting streamer', e);
+    return false;
+  }
 }
 
 async function streamerDelete(req, res) {
-    try {
-        console.log(req.params.id)
-        const streamerBool = await streamerExist(req.params.id);
-        if (!streamerBool) {
-            res.status(404).end();
-        } else {
-            const result = await streamersRepository.remove(req.params.id);
-            res.send(result);
-        }
-    } catch (e) {
-        res.status(error.status || 400).end();
+  try {
+    const streamerBool = await streamerExist(req.params.id);
+    console.log(streamerBool);
+    if (!streamerBool) {
+      res.status(404).end();
+    } else {
+      const result = await streamersRepository.remove(req.params.id);
+      res.send(result);
     }
+  } catch (e) {
+    res.status(error.status || 400).end();
+  }
 }
 
 export default {
-    getStreamers,
-    create,
-    streamerExist,
-    streamerDelete,
+  getStreamers,
+  create,
+  streamerExist,
+  streamerDelete,
 };
--- server/services/streamers-repository.js
+++ server/services/streamers-repository.js
@@ -1,72 +1,80 @@
-import {
-    response
-} from 'express';
 import esClient from './es-client';
 
 const index = 'local_streamers';
 
 const handleElasticsearchError = (error) => {
-    if (error.status === 404) {
-        throw new Error('Streamer Not Found', 404);
-    }
-    throw new Error(error.msg, error.status || 500);
+  if (error.status === 404) {
+    throw new Error('Streamer Not Found', 404);
+  }
+  throw new Error(error.msg, error.status || 500);
 };
 
-const getAll = () => esClient.search({
-    index,
-}).then(response => response).catch((error) => {
-    handleElasticsearchError(error);
-});
-
-//streamer data corect
-const store = streamerData =>
-    esClient.index({
-        index,
-        refresh: 'true',
-        body: streamerData,
-    }).then(response => response.status).catch((error) => {
-        handleElasticsearchError(error);
+const getAll = () =>
+  esClient
+    .search({
+      index: index,
+    })
+    .then((response) => response)
+    .catch((error) => {
+      handleElasticsearchError(error);
     });
 
+const store = (streamer) =>
+  esClient
+    .index({
+      index: index,
+      refresh: 'true',
+      body: { streamerName: streamer },
+    })
+    .then((response) => response.status)
+    .catch((error) => {
+      handleElasticsearchError(error);
+    });
 
-const getStreamer = streamerName => esClient.search({
-        index,
-        body: {
-            "query": {
-                "match": {
-                    "firstName": {
-                        "query": streamerName
-                    }
-                }
-            }
+const getStreamer = (streamerName) =>
+  esClient
+    .search({
+      index: index,
+      body: {
+        query: {
+          match: {
+            streamerName: {
+              query: streamerName,
+            },
+          },
         },
-    }).then(response => {
-        response;
+      },
+    })
+    .then((response) => {
+      return response;
     })
     .catch((error) => {
-        handleElasticsearchError(error);
+      handleElasticsearchError(error);
     });
 
-
-const remove = firstName => esClient.deleteByQuery({
-    index,
-    refresh: 'true',
-    body: {
-        "query": {
-            "match": {
-                "firstName": {
-                    "query": firstName
-                }
-            }
-        }
-    },
-}).then(response => response).catch((error) => {
-    handleElasticsearchError(error);
-});
+const remove = (streamerName) =>
+  esClient
+    .deleteByQuery({
+      index: index,
+      refresh: 'true',
+      body: {
+        query: {
+          match: {
+            streamerName: {
+              query: streamerName,
+            },
+          },
+        },
+      },
+    })
+    .then((response) => response)
+    .catch((error) => {
+      handleElasticsearchError(error);
+    });
 
 export default {
-    getStreamer,
-    store,
-    getAll,
-    remove,
+  getStreamer,
+  store,
+  getAll,
+  remove,
 };
